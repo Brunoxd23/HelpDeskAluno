b7a62faf00866e6c764cf7d47d32adbe37aef6a2
Brunoxd23 <monteiro_100@outlook.pt>
2024-04-08 13:33:59 -0300

    add: feat exclusao pasta page

--- app/(home)/pages/exemplo.tsx
+++
@@ -1,9 +0,0 @@
-// pages/exemplo.tsx
-export default function ExemploPage() {
-    return (
-      <div>
-        <h1>Página de Exemplo</h1>
-        <p>Esta é uma página de exemplo para testar o roteamento.</p>
-      </div>
-    );
-  }
